8112d26cafc97e15573832a05432605700a4d4be
Varun Kumar Kumaravel <45570174+varunkumar98@users.noreply.github.com>
2023-12-14 21:27:14 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,7 @@
+"""
+This is the init file of our project where it imports modules (ChartBuilder, InteractiveMap, CustomThemes) from a python package - dataviztoolkit.
+"""
+
 from dataviztoolkit.chart_builder import ChartBuilder
 from dataviztoolkit.interactive_map import InteractiveMap
 from dataviztoolkit.custom_themes import CustomThemes
